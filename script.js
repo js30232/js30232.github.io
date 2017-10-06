84dd2ae5ee690f45b968ec051733b68a4a3eb5ef
Irina <irbris@mail.ru>
2017-10-07 00:24:39 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,63 +1,50 @@
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
-var msg = new SpeechSynthesisUtterance();
 var text = document.getElementById("text");
 var speakButton = document.getElementById("speak");
-
-var rec = new SpeechRecognition();
-rec.interimResults = true; 
+var body = document.querySelector("body");
 var voice = "белый";
 
-var body = document.querySelector("body");
+var colors = {
+    "белый": "#F7F7F7",
+    "красный": "#BC382E",
+    "оранжевый": "#FF8000",
+    "желтый": "#F5BD00",
+    "зеленый": "#388D80",
+    "голубой": "#4583AA",
+    "синий цвет": "#163260",
+    "фиолетовый": "#560DA6"
+}
 
-function setOption() {
+function speak() {
+    var msg = new SpeechSynthesisUtterance();
     msg.text = text.value;
     speechSynthesis.speak(msg);    
 }
 
-rec.addEventListener("result", function(e) {
+var recognition = new SpeechRecognition();
+recognition.interimResults = true; 
+
+
+function recognitionVoice(e) {
     voice = Array.from(e.results)
-    .map(result => result[0])
-    .map(result => result.transcript)
-    .join("");
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join("");
     console.log(voice); 
     
-    if(voice.match(/белый/i)) {
-        body.style.backgroundColor = "#F7F7F7";
+    for (key in colors) {
+        if(voice === key) {
+            body.style.backgroundColor = colors[key];
+        }
     }
-    
-    if(voice.match(/красный/i)) {
-        body.style.backgroundColor = "#BC382E";
-    }
-    
-    if(voice.match(/оранжевый/i)) {
-        body.style.backgroundColor = "#FF8000";
-    }
-    
-    if(voice.match(/желтый/i)) {
-        body.style.backgroundColor = "#F5BD00";
-    }
-    
-    if(voice.match(/зеленый/i)) {
-        body.style.backgroundColor = "#388D80";
-    }
-    
-    if(voice.match(/голубой/i)) {
-        body.style.backgroundColor = "#4583AA";
-    }
-    
-    if(voice.match(/синий/i)) {
-        body.style.backgroundColor = "#163260";
-    }
-    
-    if(voice.match(/фиолетовый/i)) {
-        body.style.backgroundColor = "#560DA6";
-    }
+}
+
+recognition.addEventListener("result", recognitionVoice);
+
+recognition.addEventListener("end", function(e) {
+    recognition.start();
 });
 
-rec.addEventListener("end", function(e) {
-    rec.start();
-});
-
-speakButton.addEventListener("click", setOption);
-rec.start();
+speakButton.addEventListener("click", speak);
+recognition.start();
